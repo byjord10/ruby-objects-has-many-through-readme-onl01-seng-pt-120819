f4eb9ce9e4e458f2e5b95f346d622e49245f4bca
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-20 23:54:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,15 +1,15 @@
 class Waiter
   attr_accessor :name, :years
-  @@all = [] 
+  @@all = []
 
   def initialize(name, yrs_experience)
-    @name = name 
+    @name = name
     @yrs_experience = yrs_experience
-    @@all << self 
-  end 
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
 end
